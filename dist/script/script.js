faf6534c80dc13df3a9b120a749bb52eb5febf63
mausooq <mauchoomausooq051@gmail.com>
2025-03-02 13:25:30 +0530

    recipes updated

--- dist/script/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const navBtn = document.querySelector(".nav-btn");
-    const navLinks = document.querySelector(".nav-links");
-
-    navBtn.addEventListener("click", () => {
-        navLinks.classList.toggle("active"); 
-    });
-});
